5781579911cce274126010016c6d5efd1214635b
Irudaya Raj John Y <111234771+YIRJohnGit@users.noreply.github.com>
2024-02-06 19:55:45 +0530

    Update create.sh
    Final Version need to test

--- create.sh
+++ create.sh
@@ -101,3 +101,9 @@ read ssl_configuration_setup
 
 # Create Apache configuration
 create_apache_config "$domain_name" "$folder_location" "$php_version" "$ssl_configuration_setup"
+
+color_echo "yellow" "scp $domain_name* rza-main:/etc/httpd/sites-available/"
+color_echo "yellow" "sudo httpd -t"
+color_echo "yellow" "sudo systemctl restart httpd"
+color_echo "yellow" "sudo systemctl restart php82-php-fpm"
+color_echo "yellow" "sudo systemctl status php82-php-fpm"
