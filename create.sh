0edb8912e08573e51c5f4d43b9d8362d71543674
Irudaya Raj John Y <111234771+YIRJohnGit@users.noreply.github.com>
2024-02-06 19:33:10 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,14 +1,81 @@
 #!/bin/bash
 
 # Function to display colored messages
-color_echo() 
-{
+color_echo() {
     case "$1" in
         red)    echo -e "\e[91m$2\e[0m";;
         green)  echo -e "\e[92m$2\e[0m";;
         yellow) echo -e "\e[93m$2\e[0m";;
-        *)     echo "$2";;
+        *)      echo "$2";;
     esac
+}
+
+# Function to create Apache configuration
+create_apache_config() 
+{
+    local domain_name="$1"
+    local folder_location="$2"
+    local php_version="$3"
+    local ssl_config="$4"
+
+    local http_config="$domain_name.conf"
+    local https_config="$domain_name-le-ssl.conf"
+
+    if [ -e "$http_config" ]; then
+        color_echo "red" "Error: Configuration file for HTTP already exists. Aborting."
+        return 1
+    fi
+
+    # HTTP Configuration
+    {
+        echo "# Virtual Host Configuration for HTTP generated from YIR John Script"
+        echo "<VirtualHost *:80>"
+        echo "    ServerAdmin admin@$domain_name"
+        echo "    ServerName $domain_name"
+        echo "    ServerAlias www.$domain_name"
+        echo ""
+        echo "    DocumentRoot $folder_location/public_html"
+        echo "    DirectoryIndex index.htm index.html index.shtml index.php index.phtml"
+        echo ""
+        echo "    <Directory \"$folder_location/public_html\">"
+        echo "        Options Indexes FollowSymLinks"
+        echo "        AllowOverride All"
+        echo "        Require all granted"
+        echo "    </Directory>"
+        echo ""
+        if [ "$ssl_config" == "y" ]; then
+            echo "    Redirect permanent / https://$domain_name/"
+        fi
+        echo "</VirtualHost>"
+    } > "$http_config"
+
+    # HTTPS Configuration
+    if [ "$ssl_config" == "y" ]; then
+    {
+        echo "# Virtual Host Configuration for HTTPS generated from YIR John Script"
+        echo "<VirtualHost *:443>"
+        echo "    ServerAdmin admin@$domain_name"
+        echo "    ServerName $domain_name"
+        echo "    ServerAlias www.$domain_name"
+        echo ""
+        echo "    DocumentRoot $folder_location/public_html"
+        echo "    DirectoryIndex index.htm index.html index.shtml index.php index.phtml"
+        echo ""
+        echo "    SSLEngine on"
+        echo "    SSLCertificateFile /etc/letsencrypt/live/$domain_name/fullchain.pem"
+        echo "    SSLCertificateKeyFile /etc/letsencrypt/live/$domain_name/privkey.pem"
+        echo "    SSLCertificateChainFile /etc/letsencrypt/live/$domain_name/chain.pem"
+        echo ""
+        echo "    <Directory \"$folder_location/public_html\">"
+        echo "        Options Indexes FollowSymLinks"
+        echo "        AllowOverride All"
+        echo "        Require all granted"
+        echo "    </Directory>"
+        echo "</VirtualHost>"
+    } > "$https_config"
+    fi
+
+    color_echo "green" "Apache configuration files created successfully."
 }
 
 # Input variables
@@ -18,48 +85,19 @@ read domain_name
 color_echo "yellow" "Enter the folder location (e.g., /home/apimydomaincom):"
 read folder_location
 
-# Create Apache configuration file `/etc/httpd/sites-available/$domain_name.conf` if it doesn't exist
-config_file="./$domain_name.conf"
-
-if [ -e "$config_file" ]; then
-    color_echo "red" "Error: Configuration file already exists. Aborting."
-    exit 1 ;
-fi
-
 # Check if PHP is required
 color_echo "yellow" "Do you need PHP support for this domain? (y/n):"
 read php_confirmation
 
+php_version=""
 if [ "$php_confirmation" == "y" ]; then
-    color_echo "yellow" "Enter the PHP version (e.g.,72 / 74 / 80 / 82):"
+    color_echo "yellow" "Enter the PHP version (e.g., 72 / 74 / 80 / 82):"
     read php_version
 fi
 
-        echo "<VirtualHost *:80>" > "$config_file"
-        echo "    ServerAdmin admin@$domain_name" > "$config_file"
-        echo "    ServerName $domain_name" >> "$config_file"
-        echo "    ServerAlias www.$domain_name" >> "$config_file"
-        echo "" >> "$config_file"
-        echo "    DocumentRoot $folder_location/public_html" >> "$config_file"
-        echo "    DirectoryIndex index.htm index.html index.shtml index.php index.phtml" >> "$config_file"
-        echo "" >> "$config_file"
-        echo "    <Directory \"$folder_location/public_html\">" >> "$config_file"
-        echo "        Options Indexes FollowSymLinks" >> "$config_file"
-        echo "        AllowOverride All" >> "$config_file"
-        echo "        Require all granted" >> "$config_file"
-        echo "    </Directory>" >> "$config_file"
-        echo "" >> "$config_file"
-        if [ "$php_confirmation" == "y" ]; then
-            echo "    ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:90$php_version$folder_location/public_html/\$1" >> "$config_file"
-        fi
-        echo "" >> "$config_file"
-        echo "    ErrorLog $folder_location/logs/error.log" >> "$config_file"
-        echo "    CustomLog $folder_location/logs/access.log combined" >> "$config_file"
-        echo "" >> "$config_file"
-        echo "    Redirect permanent / https://$domain_name/" >> "$config_file"
-        echo "</VirtualHost>" >> "$config_file"
+# Check if SSL configuration is needed
+color_echo "yellow" "Do you need to Generate SSL Configuration? (y/n):"
+read ssl_configuration_setup
 
-
-    # Display success message
-    color_echo "green" "Apache configuration file created successfully at: $config_file"
-
+# Create Apache configuration
+create_apache_config "$domain_name" "$folder_location" "$php_version" "$ssl_configuration_setup"
