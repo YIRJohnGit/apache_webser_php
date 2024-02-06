ff5075d0c211d3d1aab0a813dace4c84d2f51b3a
Irudaya Raj John Y <111234771+YIRJohnGit@users.noreply.github.com>
2024-02-06 16:11:50 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -23,40 +23,41 @@ config_file="./$domain_name.conf"
 
 if [ -e "$config_file" ]; then
     color_echo "red" "Error: Configuration file already exists. Aborting."
-else
-    {
-        # Check if PHP is required
-        color_echo "yellow" "Do you need PHP support for this domain? (y/n):"
-        read php_confirmation
+    exit 1 ;
+fi
 
-        if [ "$php_confirmation" == "y" ]; then
-            color_echo "yellow" "Enter the PHP version (e.g.,72 / 74 / 80 / 82):"
-            read php_version
-        fi
+# Check if PHP is required
+color_echo "yellow" "Do you need PHP support for this domain? (y/n):"
+read php_confirmation
 
-        echo "<VirtualHost *:80>"
-        echo "    ServerAdmin admin@$domain_name"
-        echo "    ServerName $domain_name"
-        echo "    ServerAlias www.$domain_name"
-        echo ""
-        echo "    DocumentRoot $folder_location/public_html"
-        echo "    DirectoryIndex index.htm index.html index.shtml index.php index.phtml"
-        echo ""
-        echo "    <Directory \"$folder_location/public_html\">"
-        echo "        Options Indexes FollowSymLinks"
-        echo "        AllowOverride All"
-        echo "        Require all granted"
-        echo "    </Directory>"
+if [ "$php_confirmation" == "y" ]; then
+    color_echo "yellow" "Enter the PHP version (e.g.,72 / 74 / 80 / 82):"
+    read php_version
+fi
+
+        echo "<VirtualHost *:80>" > 
+        echo "    ServerAdmin admin@$domain_name" > "$config_file"
+        echo "    ServerName $domain_name" >> "$config_file"
+        echo "    ServerAlias www.$domain_name" >> "$config_file"
+        echo "" >> "$config_file"
+        echo "    DocumentRoot $folder_location/public_html" >> "$config_file"
+        echo "    DirectoryIndex index.htm index.html index.shtml index.php index.phtml" >> "$config_file"
+        echo "" >> "$config_file"
+        echo "    <Directory \"$folder_location/public_html\">" >> "$config_file"
+        echo "        Options Indexes FollowSymLinks" >> "$config_file"
+        echo "        AllowOverride All" >> "$config_file"
+        echo "        Require all granted" >> "$config_file"
+        echo "    </Directory>" >> "$config_file"
         if [ "$php_confirmation" == "y" ]; then
-            echo "    ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:90$php_version$folder_location/public_html/\$1"
+            echo "    ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:90$php_version$folder_location/public_html/\$1" >> "$config_file"
         fi
-        echo ""
-        echo "    ErrorLog $folder_location/logs/error.log"
-        echo "    CustomLog $folder_location/logs/access.log combined"
-        echo ""
-        echo "    Redirect permanent / https://$domain_name/"
-        echo "</VirtualHost>"
-    } > "$config_file"
+        echo "" >> "$config_file"
+        echo "    ErrorLog $folder_location/logs/error.log" >> "$config_file"
+        echo "    CustomLog $folder_location/logs/access.log combined" >> "$config_file"
+        echo "" >> "$config_file"
+        echo "    Redirect permanent / https://$domain_name/" >> "$config_file"
+        echo "</VirtualHost>" >> "$config_file"
+
 
     # Display success message
     color_echo "green" "Apache configuration file created successfully at: $config_file"
